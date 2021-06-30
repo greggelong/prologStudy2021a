5448a34cd2069422e5113a92ab337619cca2ef82
Greg K <gkreisman_cep@qq.com>
2021-06-30 12:57:31 +0800

    added extra postion to right and left

--- houseCfg.pl
+++ houseCfg.pl
@@ -6,12 +6,14 @@ s(X):- X = [_,_,_],
 	member(house(green,_,_),X),
 	member(house(_,_,snail),X),
 	member(house(_,_,zebra),X),
-	%% japanese lives to the  right of snail
+	%% japanese lives to the  right of snail  Note I needed to add round bracketes
 	(X = [house(_,_,snail),house(_,japanese,_),_];
-	X = [_,house(_,_,snail),house(_,japanese,_)]),
+	X = [_,house(_,_,snail),house(_,japanese,_)];
+	X = [house(_,_,snail),_,house(_,japanese,_)]),
 	%% snail keeper lives to the left of blue house
 	(X = [house(_,_,snail),house(blue,_,_),_];
-	X = [_,house(_,_,snail),house(blue,_,_)]).
+	X = [_,house(_,_,snail),house(blue,_,_)];
+	X = [house(_,_,snail),_,house(blue,_,_)]).
 
 
 

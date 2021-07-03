18d589146d7ddde8b36355e083c5ca4c0bea21bb
Greg K <gkreisman_cep@qq.com>
2021-07-03 22:09:58 +0800

    ex 8 grammars DCG

---
+++ ex8p1DFG.pl
@@ -0,0 +1,25 @@
+s --> np(plural), vp(plural).
+s --> np(singular), vp(singular).
+
+np(singular) --> det(singular), n(singular).
+np(plural) --> det(plural), n(plural).
+
+vp(singular) --> v(singular), np(_).
+vp(plural) --> v(plural), np(_).
+
+vp(singular) --> v(singular).
+vp(plural) --> v(plural).
+
+det(singular) --> [the].
+det(plural) --> [the].
+det(singular) --> [a].
+
+n(singular) --> [woman].
+n(plural) --> [women].
+n(singular) --> [man].
+n(plural) --> [men].
+ 
+
+v(singular)-->[shoots].
+v(plural)-->[shoot].
+
